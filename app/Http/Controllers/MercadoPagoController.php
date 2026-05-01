32fadc718b355c5a28f55eff6c2d9daabe2118a8
adrielprimeiro <adrielprimeiro@gmail.com>
2026-05-01 13:23:05 -0300

    Fix: Import DB facade in MercadoPagoController to prevent Class not found error

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Pedido;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class MercadoPagoController extends Controller
 {
