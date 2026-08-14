3b816e2c9e923c58c7c8c98262a4e3de701cd40c
adrielprimeiro <adrielprimeiro@gmail.com>
2026-08-14 09:28:01 -0300

    chore: final cleanup

--- check_column.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$hasColumn = \Illuminate\Support\Facades\Schema::hasColumn('avaliacoes', 'pessoa_id');
-echo "Has pessoa_id: " . ($hasColumn ? "YES" : "NO") . "\n";
