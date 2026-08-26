e68ac89728c57b4a7c81ab7d6ea557c1576662d7
adrielprimeiro <adrielprimeiro@gmail.com>
2026-08-26 16:44:55 -0300

    Fix missing route name for chat-ia.prefetch-greeting

--- routes/web.php
+++ routes/web.php
@@ -876,6 +876,7 @@ Route::post('/api/chat-ia/send', [\App\Http\Controllers\AiAssistantController::c
 Route::post('/api/chat-ia/greeting', [\App\Http\Controllers\AiAssistantController::class, 'getGreeting'])
     ->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
 Route::post('/api/chat-ia/prefetch-greeting', [\App\Http\Controllers\AiAssistantController::class, 'prefetchGreeting'])
-    ->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
+    ->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])
+    ->name('chat-ia.prefetch-greeting');
 Route::get('/api/chat-ia/history', [\App\Http\Controllers\AiAssistantController::class, 'getHistory']);
 Route::get('/portal/ajuda', [\App\Http\Controllers\AiAssistantController::class, 'portalChat'])->middleware('auth')->name('portal.ajuda');
