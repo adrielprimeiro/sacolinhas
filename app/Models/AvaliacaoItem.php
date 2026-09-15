d542747df8d28a1f43f29717c8005c2f4f3ccec6
adrielprimeiro <adrielprimeiro@gmail.com>
2026-09-15 10:59:46 -0300

    Fix: backend sobrescrevia o preço de venda manualmente editado pelo usuário

--- app/Models/AvaliacaoItem.php
+++ app/Models/AvaliacaoItem.php
@@ -97,11 +97,14 @@ class AvaliacaoItem extends Model
         }
 
         // 2. Preço de venda = Base * (Porcentagem / 100), arredondado para múltiplo de 5 (se não for preço fixo)
-        if ($this->is_fixed_price) {
-            $this->preco_venda = (float) $this->preco_base;
-        } else {
-            $preco_calculado = ((float) $this->preco_base * ($porcentagem / 100.00));
-            $this->preco_venda = round($preco_calculado / 5) * 5;
+        // Apenas recalculamos se estiver zerado/nulo, para respeitar edições manuais do usuário
+        if (empty($this->preco_venda) || $this->preco_venda <= 0) {
+            if ($this->is_fixed_price) {
+                $this->preco_venda = (float) $this->preco_base;
+            } else {
+                $preco_calculado = ((float) $this->preco_base * ($porcentagem / 100.00));
+                $this->preco_venda = round($preco_calculado / 5) * 5;
+            }
         }
 
         // 3. Taxa de curadoria (10 é 0, 1 é 10, outros são 10 - nota)
